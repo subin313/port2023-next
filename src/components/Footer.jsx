4f1d7b372e119b3286882de8b7efbf43295a94d3
subin313 <82709790+subin313@users.noreply.github.com>
2023-11-07 21:44:09 +0900

    1차 완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,44 @@
 import React from "react";
-
+import { footerText } from "@/constants";
 const Footer = () => {
 return(
-    <div>Footer</div>
+    <footer id="footer">
+        <div className="footer__inner">
+            <div className="footer__text">
+                <span>subinggood</span>
+                <span>Fortpolio</span>
+            </div>
+            <div className="footer__info">
+            <div className="left">
+                <div className="title">
+                    <a href="#">
+                        sign up
+                    </a>
+                </div>
+                <p>다양한 프레임워크를 이용한 웹 사이트</p>
+            </div>
+            <div className="right">
+                <h3>social</h3>
+                <ul>
+                        {footerText.map((footer,key)=>(
+                    <li>
+                            <a href={footer.link} target="_blank" rel="noopener noreferrer">
+                            {footer.title}
+                        </a>
+                        <em>{footer.desc}</em>
+                    </li>
+                            ))}
+                    
+                </ul>
+            </div>
+            </div>
+            <div className="footer__right">
+                @subinggood<br/>
+                @ 2023 website portfolio
+            </div>
+        </div>
+
+    </footer>
 )
 }
 
--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { portText } from "@/constants"; 
+import Image from "next/image";
 
 const Port = () =>{
-    <div>Port</div>
+    return (
+    <section id="port">
+        <div className="port__inner">
+            <h2 className="port__title">
+                portfolio<em>포폴 작업물</em>
+            </h2>
+            <div className="port__wrap">
+                {portText.map((port,key)=>(
+
+                    <article className={`port__items p${key +1 }`} key={key}>
+                <span className="num">{port.num}.</span>
+                <a className="img" href={port.code} target="_blank">
+                <Image src={port.img} alt={port.name} width={420} height={262} />
+            </a>
+            <h3 className="title">{port.title}</h3>
+            <p className="desc">
+            {port.desc}</p>
+           <a href={port.view} target="_blank" className="site" >사이트 보기</a>
+            </article>
+           ))}
+            </div>
+        </div>
+    </section>
+)
 }
 
 export default Port;
